d73d16e0b1aa9dd0cac95ff817c752018e96d8e5
aloksharma10 <aloksharma10969@gmail.com>
2022-10-05 18:52:16 +0530

    HomeDesign

--- pages/index.js
+++ pages/index.js
@@ -33,34 +33,39 @@ export default function Home() {
         </div>
       </div>
 
-      <section class="text-gray-600 body-font overflow-hidden">
+      <section class="text-gray-600 body-font">
         <div class="container px-5 py-24 mx-auto">
-          <div class="-my-8 divide-y-2 divide-gray-100">
-          <div class="py-8 flex flex-wrap md:flex-nowrap">
-              <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span class="font-semibold title-font text-gray-700">CATEGORY</span>
-                <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+          <div class="flex flex-wrap -m-4">
+            <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
+              <div class="h-full text-center">
+                <img alt="testimonial" class="w-full h-80 mb-8 object-cover object-center rounded-xl inline-block border-2 border-gray-200 bg-gray-100" src="/img/a1.png"/>
+                  <p class="leading-relaxed">SPEED is one of the biggest events of its kind in Delhi-NCR region. The event was inaugurated by Mr. Venkatesh Sarvasiddhi, Head-Academic Initi/ atives & Microsoft Innovation Center, Microsoft, and Prof. (Dr.) Deependra Kumar Jha, Vice- Chancellor, GD Goenka University. Mr. Venkatesh was immensely impressed by this event; the number of participations, the variety and quality of projects presented here, and the infrastructure and level of organization in this event.</p>
+                  <span class="inline-block h-1 w-10 rounded bg-red-500 mt-6 mb-4"></span>
               </div>
-              <div class="md:flex-grow">
-                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Bitters hashtag waistcoat fashion axe chia unicorn</h2>
-                <p class="leading-relaxed">SPEED is one of the biggest events of its kind in Delhi-NCR region. The event was inaugurated by Mr. Venkatesh Sarvasiddhi, Head-Academic Initiatives & Microsoft Innovation Center, Microsoft, and Prof. (Dr.) Deependra Kumar Jha, Vice- Chancellor, GD Goenka University. Mr. Venkatesh was immensely impressed by this event; the number of participations, the variety and quality of projects presented here, and the infrastructure and level of organization in this event.</p>
-                <a class="text-red-500 inline-flex items-center mt-4">Learn More
-                  <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
+            </div>
+            <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
+              <div class="h-full text-center">
+                <img alt="testimonial" class="w-full h-80 mb-8 object-cover object-center rounded-xl inline-block border-2 border-gray-200 bg-gray-100" src="/img/3.jpg"/>
+                  <p class="leading-relaxed">After inauguration, the exhibition was open for general viewing and evaluation by judges. The project evaluation team consisted of Mr. Devendra/  Burman, Vice President, Accenture and Mr. Amit Sharma, Head, Phoenix (India) Pvt. Ltd. There was huge excitement among participants and the visitors.</p>
+                  <span class="inline-block h-1 w-10 rounded bg-red-500 mt-6 mb-4"></span>
+              </div>
+            </div>
+            <div class="lg:w-1/3 lg:mb-0 p-4">
+              <div class="h-full text-center">
+                <img alt="testimonial" class="w-full h-80 mb-8 object-cover object-center rounded-xl inline-block border-2 border-gray-200 bg-gray-100" src="/img/7a.png"/>
+                  <p class="leading-relaxed">SPEED  will be organized at the sprawling campus of GD Goenka University which is located at the serene foothills of Aravali near Gurgaon at Gurgaon-Sohna Road. All science and engineering students from schools/universities/colleges are welcome to participate in this exciting competition and win exciting prizes apart from having fun!</p>
+                  <span class="inline-block h-1 w-10 rounded bg-red-500 mt-6 mb-4"></span>
               </div>
             </div>
           </div>
         </div>
       </section>
 
-      <div className="container mx-auto">
+      {/* <div className="container mx-auto">
         <section className="text-gray-600 body-font">
           <div className="container mx-auto flex px-5 py-10 items-center justify-center flex-col">
             <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
-            <div className="text-center lg:w-2/3 w-full">
+            <div className="text-center lg:w-2/3 w-full">/  
               <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Microdosing synth tattooed vexillologist</h1>
               <p className="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
               <div className="flex justify-center">
@@ -70,7 +75,7 @@ export default function Home() {
             </div>
           </div>
         </section>
-      </div>
+      </div> */}
     </>
   )
 }
