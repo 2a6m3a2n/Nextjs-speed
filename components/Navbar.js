92e3ab634802f15860db0a3daf241b3c63879ebd
aloksharma10 <aloksharma10969@gmail.com>
2022-10-05 21:10:34 +0530

    NavChange

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,18 +6,17 @@ function Navbar() {
     return (
         <>
             <Marquee gradient={false} speed={100} className="bg-red-500 font-semibold text-white" behavior="" direction="left">Coming Soon</Marquee>
-            <header className="text-gray-600 body-font">
+            <header className="text-gray-600 body-font h-52 md:h-auto w-full z-10 sticky bg-white top-0 border-b border-grey-light shadow-md">
                 <div className="mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                     <a className="title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                        {/* eslint-disable-next-line @next/next/no-img-element */}
                         <img style={{ width: "26rem" }} src={"img/speedLogo.png"} alt="" srcSet="" />
                         <p className='text-sm'>Providing Engineering Solution for Nation Building</p>
                     </a>
                     <nav className={`md:ml-auto flex flex-wrap items-center text-base justify-center`}>
-                        <Link href={'/'}><a className="mr-5 hover:text-red-500 text-black text-base font-semibold hover:border-b-2 hover:border-red-700">Home</a></Link>
-                        <Link href={'/award'}><a className="mr-5 hover:text-red-500 text-black text-base font-semibold hover:border-b-2 hover:border-red-700">Best Award</a></Link>
-                        <Link href={'/prize'}><a className="mr-5 hover:text-red-500 text-black text-base font-semibold hover:border-b-2 hover:border-red-700">Prize</a></Link>
-                        <Link href={'/contact'}><a className="mr-5 hover:text-red-500 text-black text-base font-semibold hover:border-b-2 hover:border-red-700 ">Contact Us</a></Link>
+                        <Link href={'/'}><a className="mr-5 text-sm hover:text-red-500 text-black md:text-base font-semibold hover:border-b-2 hover:border-red-700">Home</a></Link>
+                        <Link href={'/award'}><a className="mr-5 text-sm hover:text-red-500 text-black md:text-base font-semibold hover:border-b-2 hover:border-red-700">Best Award</a></Link>
+                        <Link href={'/prize'}><a className="mr-5 text-sm hover:text-red-500 text-black md:text-base font-semibold hover:border-b-2 hover:border-red-700">Prize</a></Link>
+                        <Link href={'/contact'}><a className="mr-5 text-sm hover:text-red-500 text-black md:text-base font-semibold hover:border-b-2 hover:border-red-700 ">Contact Us</a></Link>
                     </nav>
                     <Link href={'/apply'}>
                         <button className="inline-flex items-center border-0 py-1 px-3 focus:outline-none bg-red-600 rounded text-base mt-4 md:mt-0 text-white">Apply Now
