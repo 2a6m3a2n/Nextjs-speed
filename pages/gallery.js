4ae1e5f85ec4898f016188519e6785dab8c3164d
aloksharma10 <aloksharma10969@gmail.com>
2022-10-09 19:29:13 +0530

    Done

--- pages/gallery.js
+++ pages/gallery.js
@@ -14,15 +14,15 @@ function Gallery({ imgs }) {
     return (
         <>
         <h2 className='text-center text-4xl font-semibold my-6'>Our Memories</h2>
-            {viewImg && <div  className="w-full h-full md:h-[32rem] bg-red-50 fixed top-60 md:top-40 z-10 bg-opacity-60 pt-16 p-4 md:pt-0  md:flex justify-center items-center overflow-hidden">
-                <Image key={viewIndex} width={500} height={400} src={`/img/${viewImg}`} alt="" srcSet="" />
+            {viewImg && <div  className="w-full h-full bg-red-50 fixed top-0 z-10 bg-opacity-60 pt-16 p-4 md:pt-0  md:flex justify-center items-center overflow-hidden">
+                <img width={900} key={viewIndex} src={`/img/memories/${viewImg}`} alt="" srcSet="" />
                 <button className='absolute text-red-400 font-bold text-3xl top-3 right-3' onClick={() => { setViewImag(null) }}>X</button>
             </div>}
             <div className="container flex items-center justify-center mx-auto cursor-pointer">
-                <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 px-2 '>
+                <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2 px-2'>
                     {img && img.map((imgtitle, index) => {
                         return (
-                            <Image key={index} width={300} height={250} src={`/img/${imgtitle}`} alt="" srcSet="" onClick={() => { handleImage(imgtitle, index) }} />
+                            <img key={index}  src={`/img/memories/${imgtitle}`} alt="" srcSet="" onClick={() => { handleImage(imgtitle, index) }} />
                         )
                     })}
                 </div>
@@ -32,7 +32,8 @@ function Gallery({ imgs }) {
 }
 
 export async function getStaticProps(context) {
-    let imgs = await fs.promises.readdir('public/img')
+    let imgs = await fs.promises.readdir('public/img/memories')
+    console.log(imgs)
     return {
         props: { imgs }
     }
